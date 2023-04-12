e72a0fef72335fdd316786aec1e4d28e9cc9dffe
shaikjamal99 <shaikjamal191@gmail.com>
2023-04-12 16:55:48 +0530

    EmployeeWageProblem

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace EmployeeWageComputation99
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = 1;
+            //welcome message
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            //UC1-EmployeeAttendance
+            Random random = new Random();
+            int employeeAttendance = random.Next(0, 2); //0 or 1
+            if (employeeAttendance == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
+        }
+    }
+}
